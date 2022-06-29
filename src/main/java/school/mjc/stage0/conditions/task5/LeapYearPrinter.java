e92cc89e44319a2c9c998dafb46ac3c7d138433f
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-06-29 14:08:22 +0400

    first_try

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,6 +1,26 @@
 package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
-    public void isLeapYear(int year) {
+    public static void isLeapYear(int year) {
+        int reminder = 0;
+
+        if (( year % 400 == 0)|| (( year % 4 == 0 ) &&( year % 100 != 0))) {
+            reminder = 1;
+        }
+        switch (reminder) {
+            case 1:
+                System.out.println("leap");
+                break;
+            case 0:
+                System.out.println("not leap");
+                break;
+            default:
+                System.out.println("wrong input");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        isLeapYear(2004);
     }
 }
